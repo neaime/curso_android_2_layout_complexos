70b5ae8fa13ee8fe95b594261ea5b838a46e9136
Felipe Neaime <fneaime@gmail.com>
2021-01-05 23:34:49 -0300

    First Commit

---
+++ app/src/main/java/br/com/cnnovelty/felipeneaimeviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.com.cnnovelty.felipeneaimeviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.cnnovelty.felipeneaimeviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
